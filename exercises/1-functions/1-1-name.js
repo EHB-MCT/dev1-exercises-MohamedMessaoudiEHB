ba4d4fd97f4dc2e6ae6935f714ff52638c6d4771
Mohamed Messaoudi <mohamed.messaoudi@student.ehb.be>
2022-10-05 21:54:11 +0200

    excercise name

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -1,1 +1,39 @@
 "use strict";
+drawLine();
+
+function drawLine() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+ function drawname(){} 
+//letter M
+
+    context.beginPath();
+    context.moveTo(300, 300);
+    context.lineTo(300, 500);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(300, 300);
+    context.lineTo(450, 350 );
+    context.lineTo(600, 300);
+    context.lineTo(600, 500 )
+    context.stroke();
+
+    //letter o
+
+    context.beginPath();
+    context.moveTo(800,500);
+    context.lineTo(700, 400);
+    context.lineTo(800, 300);
+    context.lineTo(900, 400);
+    context.lineTo(800, 500)
+    context.stroke();
+
+    
+
+
+ }
