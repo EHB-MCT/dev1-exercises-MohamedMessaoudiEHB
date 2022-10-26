b43181156328604674a1aef7ce1cf0360accd04d
mohamed messaoudi <mohamed.messaoudi@student.ehb.be>
2022-10-26 12:21:52 +0200

    4 excercises

--- exercises/more lines.js
+++ exercises/more lines.js
@@ -2,25 +2,30 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
-let width=context.canvas.width;
-let height= context.canvas.height;
+let width = context.canvas.width;
+let height = context.canvas.height;
 
 draw();
 
-function draw(){
-context.lineWidth = 2;
-context.strokeStyle = "white";
-context.fillStyle = "orange";
-context.fillRect(0,0, width, height);
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "orange";
+    context.fillRect(0, 0, width, height);
 
 }
-let space=10;
+let space = 10;
+let xspacing = width / 60;
 drawStreep();
 
-function drawStreep(){
-for (let i=0;i<(width/i);i++){
-Utils.drawLine(0+(space*i),0,width-(space*i),height);
-Utils.drawLine(0,0+(space*i),width,height-(space*i));
-}
+function drawStreep() {
+    for (let i = 0; i < 60; i++) {
+        Utils.drawLine(0 + (xspacing * i), 0, width - (xspacing * i), height);
+    }
+    let spacing = height / 30;
+    for (let i = 0; i <= 30; i++) {
+        Utils.drawLine(0, 0 + (spacing * i), width, height - (spacing * i));
+
+    }
 
 }
